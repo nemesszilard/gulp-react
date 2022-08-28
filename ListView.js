f577a00f3eef616838cca5cee2f81103c7d0c8fb
nemesszilard <39024992+nemesszilard@users.noreply.github.com>
2022-08-28 17:23:54 +0200

    Add files via upload

---
+++ ListView.js
@@ -0,0 +1,11 @@
+import React, {PureComponent} from "react";
+
+class ListView extends PureComponent {
+    render() {
+        return(
+            <div>Test Component View! :-) :-)</div>
+        );
+    }
+}
+
+export default ListView;
---
+++ TestComponent.js
@@ -0,0 +1,11 @@
+import React, {PureComponent} from "react";
+
+class TestComponent extends PureComponent {
+    render() {
+        return(
+            <div>Test Component! :-) :-)</div>
+        );
+    }
+}
+
+export default TestComponent;
